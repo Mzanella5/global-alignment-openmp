5c021c82c00e055d530a1f4571ff06be73f3d231
Maicon Zanella <mz_maicon@hotmail.com>
2023-11-27 20:20:08 -0300

    Remove some debug logs

--- time_tests.sh
+++ time_tests.sh
@@ -21,16 +21,16 @@ done
 
 #45k
 #================================================================================================================
-# for ((j = 1; j <= cores; j++)); do
-#     sample="./main samples/45k1.fna samples/45k2.fna --match 2 --mismatch -3 --gap -5 --gap_seq -2 --blocks $j --threads $j"
-#     for ((i = 1; i <= times; i++)); do
-#         message="Threads: $j Tamanho: 45k Execução $i/3"
-#         echo $message
-#         echo $message >> $saida
-#         echo $sample >> $saida && $sample >> $saida 2>&1
-#         echo "" >> $saida
-#     done
-# done
+for ((j = 1; j <= cores; j++)); do
+    sample="./main samples/45k1.fna samples/45k2.fna --match 2 --mismatch -3 --gap -5 --gap_seq -2 --blocks $j --threads $j"
+    for ((i = 1; i <= times; i++)); do
+        message="Threads: $j Tamanho: 45k Execução $i/3"
+        echo $message
+        echo $message >> $saida
+        echo $sample >> $saida && $sample >> $saida 2>&1
+        echo "" >> $saida
+    done
+done
 
 #60k
 #================================================================================================================
@@ -47,16 +47,16 @@ done
 
 #80k
 #================================================================================================================
-# for ((j = 1; j <= cores; j++)); do
-#     sample="./main samples/80k1.fna samples/80k2.fna --match 2 --mismatch -3 --gap -5 --gap_seq -2 --blocks $j --threads $j"
-#     for ((i = 1; i <= times; i++)); do
-#         message="Threads: $j Tamanho: 80k Execução $i/3"
-#         echo $message
-#         echo $message >> $saida
-#         echo $sample >> $saida && $sample >> $saida 2>&1
-#         echo "" >> $saida
-#     done
-# done
+for ((j = 1; j <= cores; j++)); do
+    sample="./main samples/80k1.fna samples/80k2.fna --match 2 --mismatch -3 --gap -5 --gap_seq -2 --blocks $j --threads $j"
+    for ((i = 1; i <= times; i++)); do
+        message="Threads: $j Tamanho: 80k Execução $i/3"
+        echo $message
+        echo $message >> $saida
+        echo $sample >> $saida && $sample >> $saida 2>&1
+        echo "" >> $saida
+    done
+done
 
 #90k
 #================================================================================================================
@@ -73,28 +73,28 @@ done
 
 #100k
 #================================================================================================================
-# for ((j = 1; j <= cores; j++)); do
-#     sample="./main samples/100k1.fna samples/100k2.fna --match 2 --mismatch -3 --gap -5 --gap_seq -2 --blocks $j --threads $j"
-#     for ((i = 1; i <= times; i++)); do
-#         message="Threads: $j Tamanho: 100k Execução $i/3"
-#         echo $message
-#         echo $message >> $saida
-#         echo $sample >> $saida && $sample >> $saida 2>&1
-#         echo "" >> $saida
-#     done
-# done
+for ((j = 1; j <= cores; j++)); do
+    sample="./main samples/100k1.fna samples/100k2.fna --match 2 --mismatch -3 --gap -5 --gap_seq -2 --blocks $j --threads $j"
+    for ((i = 1; i <= times; i++)); do
+        message="Threads: $j Tamanho: 100k Execução $i/3"
+        echo $message
+        echo $message >> $saida
+        echo $sample >> $saida && $sample >> $saida 2>&1
+        echo "" >> $saida
+    done
+done
 
 #130k
 #================================================================================================================
-# for ((j = 1; j <= cores; j++)); do
-#     sample="./main samples/130k1.fna samples/130k2.fna --match 2 --mismatch -3 --gap -5 --gap_seq -2 --blocks $j --threads $j"
-#     for ((i = 1; i <= times; i++)); do
-#         message="Threads: $j Tamanho: 130k Execução $i/3"
-#         echo $message
-#         echo $message >> $saida
-#         echo $sample >> $saida && $sample >> $saida 2>&1
-#         echo "" >> $saida
-#     done
-# done
+for ((j = 1; j <= cores; j++)); do
+    sample="./main samples/130k1.fna samples/130k2.fna --match 2 --mismatch -3 --gap -5 --gap_seq -2 --blocks $j --threads $j"
+    for ((i = 1; i <= times; i++)); do
+        message="Threads: $j Tamanho: 130k Execução $i/3"
+        echo $message
+        echo $message >> $saida
+        echo $sample >> $saida && $sample >> $saida 2>&1
+        echo "" >> $saida
+    done
+done
 
 echo "Execuções concluídas. Resultados salvos em $saida"
